4bb515b8415afd7e6fd1e85ad53b47537a1dda39
RunoWazlib <ethanrog01@gmail.com>
2025-07-09 11:10:26 -0500

    Tests for project_settings_new_version_view; Bug fixes in project_settings_new_version_view; TODO - Version model field "release_date" + unset_date_selector template refactor;

--- tracker/frontend/forms/project/version/new_version_form.py
+++ tracker/frontend/forms/project/version/new_version_form.py
@@ -5,6 +5,6 @@ class NewVersionDataForm(forms.Form):
     name = forms.CharField()
     description = forms.CharField(required=False)
     label = forms.CharField()
-    # release_date = forms.DateField(required=False, widget=forms.SelectDateWidget())  # TODO: The unset_date_selector template is hardcoded to match the project form... needs a refactor to be useful here for the verison template(s)
+    release_date = forms.DateField(required=False, widget=forms.SelectDateWidget())  # TODO: The unset_date_selector template is hardcoded to match the project form... needs a refactor to be useful here for the version template(s)
     is_active = forms.BooleanField()
     project_id = forms.CharField(required=False, widget=forms.HiddenInput())  # must be put in the form for the Project's New Component POST to work
--- tracker/frontend/views/project/project/project_settings_new_version_view.py
+++ tracker/frontend/views/project/project/project_settings_new_version_view.py
@@ -19,15 +19,19 @@ def get_project(logged_in_user, project_id):
     except ValueError:
         try:
             return logged_in_user.list_projects().get(label__current__label=project_id)
-        except project_models.Project.DoesNotExist:
-            return None
+        except:
+            raise project_models.Project.DoesNotExist
 
 
 def handle_post(request, logged_in_user, project_id):
     received_new_version_form = new_version_form.NewVersionDataForm(request.POST, request.FILES)
-    project = get_project(logged_in_user, project_id)
 
-    if project:  # ALWAYS MAKE SURE USER CAN SEE THE PROJECT FIRST
+    try:  # ALWAYS MAKE SURE USER CAN SEE THE PROJECT FIRST
+        project = get_project(logged_in_user, project_id)
+    except project_models.Project.DoesNotExist:
+        messages.error(request, 'Permission denied.')
+        return redirect("projects")
+    else:
         if received_new_version_form.is_valid():
             version_data = version_models.VersionData.objects.create(
                 created_by=logged_in_user,
@@ -42,17 +46,11 @@ def handle_post(request, logged_in_user, project_id):
                 current=version_data,
                 project=project
                 )
-
             messages.success(request, ('Your version was successfully added!'))
         else:
-            project = get_project(logged_in_user, project_id)
             messages.error(request, 'Invalid data received. Please try again.')
-    else:
-        messages.error(request, 'Permission denied.')
 
     project_dict = model_to_dict(project.current)
-    if project.label.current.label:
-        project_dict['label'] = project.label.current.label
     form = project_form.ProjectDataForm(project_dict)
     repositories = project.git_repositories.all()
     components = project.component_set.all()
@@ -75,10 +73,7 @@ def handle_post(request, logged_in_user, project_id):
 
 @login_required
 def new_version(request, project_id=None):
-    try:
-        logged_in_user = core_user_models.CoreUser.active_objects.get(user__username=request.user)
-    except core_user_models.CoreUser.DoesNotExist:
-        return redirect("logout")
+    logged_in_user = core_user_models.CoreUser.active_objects.get(user__username=request.user)
 
     if request.method == "POST":
         return handle_post(request, logged_in_user, project_id)
