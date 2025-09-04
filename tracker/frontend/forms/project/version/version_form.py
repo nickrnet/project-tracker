3c4982e491967cb168ef8b16424246fdbc006116
Nick Rogers <nrogers@gmail.com>
2025-09-04 18:00:29 -0500

    refactor UI date selectors to be more generic
    refactor today_date_selector and usages;
    refactor unset_date_selector and usages;

--- tracker/frontend/forms/project/version/version_form.py
+++ tracker/frontend/forms/project/version/version_form.py
@@ -5,5 +5,5 @@ class VersionDataForm(forms.Form):
     name = forms.CharField()
     description = forms.CharField(required=False)
     label = forms.CharField()
-    release_date = forms.DateField(required=False, widget=forms.SelectDateWidget())  # TODO: The unset_date_selector template is hardcoded to match the project form... needs a refactor to be useful here for the verison template(s)
+    release_date = forms.DateField(required=False, widget=forms.SelectDateWidget())
     is_active = forms.BooleanField(required=False)
